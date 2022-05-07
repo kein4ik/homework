8524b063f3fc537a80eafefec85befaf763a4f88
kein4ik <vip.crazysven@gmail.com>
2022-05-07 13:56:07 -0400

    general improvements

---
+++ main.js
@@ -0,0 +1,19 @@
+//document.getElementById("box").innerText = 5
+var i = 0;
+function increment() {
+    if (i == 10) {
+        alert("rate only out of 10")
+        return null
+    }
+    document.getElementById('box').value = ++i;
+}
+
+
+
+function decrement() {
+    if (i < 0) {
+        alert("this cant be negotive")
+        return null;
+    }
+    document.getElementById("box").value = --i;
+}
